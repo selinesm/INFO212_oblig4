0b5a65802513092b62599aa27ed82e066f592f55
EWT98 <erlend13@hotmail.no>
2023-10-17 15:04:29 +0200

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from models import Employee
+from flask import Flask, request, jsonify
 
 views = Blueprint("views", __name__)
 
@@ -7,3 +9,22 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template("home.html")
+
+
+
+
+
+
+@staticmethod
+def create(name, address, branch):
+    return Employee(name, address, branch)
+        
+def read(self):
+    return f"Name: {self.name}, address: {self.address}, branch: {self.branch}"
+
+def update(self):
+    pass
+
+@staticmethod
+def delete(employee):
+    del employee
